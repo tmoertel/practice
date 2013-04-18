60e51ee9949eda8701b17473b8ff3c2d85a6e8eb
Tom Moertel <tom@moertel.com>
2013-04-18 13:37:43 -0400

    Start on different approach to "Treasures" prob

--- google-code-jam/2013/qual-D-treasure/treasure.py
+++ google-code-jam/2013/qual-D-treasure/treasure.py
@@ -9,7 +9,7 @@ https://code.google.com/codejam/contest/2270488/dashboard#s=p3
 
 """
 
-from bisect import bisect_left
+from collections import Counter, defaultdict
 import fileinput
 
 def main():
@@ -18,28 +18,44 @@ def main():
         print 'Case #%r: %s' % (i, s)
 
 def solve(problem):
-    K, N, starting_keys, chests = problem
+    K, _N, starting_keys, chests = problem
 
-    def search(keys, locked_chests, seq):
-        if not locked_chests:
+    keys_in_chest = dict((i, keys_i) for i, (_, keys_i) in chests.iteritems())
+
+    chests_unlocked_by = defaultdict(set)
+    key_debt = Counter()
+    for i, (T_i, _) in chests.iteritems():
+        chests_unlocked_by[T_i].add(i)
+        key_debt[T_i] += 1
+
+    chests = set(chests)
+    keys = Counter(starting_keys)
+
+    def search(keys, key_debt, chests, seq):
+        print '%ssearch(seq=%r, keys=%r, debt=%r, chests=%r)' % (
+            ' ' * len(seq), seq, keys, key_debt, chests)
+        if not chests:
             return seq
-        if not keys:
-            return None
-        for i in locked_chests:
-            unlock_key, keys_i = chests[i]
-            k = bisect_left(keys, unlock_key)
-            if k != len(keys) and keys[k] == unlock_key:
-                soln = search(sorted(keys_i + keys[:k] + keys[k+1:]),
-                              [j for j in locked_chests if j != i],
-                              seq + [i])
-                if soln is not None:
-                    return soln
+        for key in keys:
+            for chest in sorted(chests & chests_unlocked_by[key]):
+                if key in keys_in_chest[chest] or keys[key] >= key_debt[key]:
+                    soln = search(keys - Counter([key]) + keys_in_chest[chest],
+                                  key_debt - Counter([key]),
+                                  chests - set([chest]),
+                                  seq + [chest])
+                    if soln is not None:
+                        return soln
 
-    soln = search(starting_keys, sorted(chests), [])
+    soln = search(keys, key_debt, chests, [])
 
     if soln is None:
         return 'IMPOSSIBLE'
     return ' '.join(str(i) for i in soln)
+
+
+def selections(xs):
+    for i, x in enumerate(xs):
+        yield x, xs[:i] + xs[i+1:]
 
 
 
@@ -54,7 +70,7 @@ def read_problem(lines):
     def read_chest():
         ints = read_ints(lines)
         T_i, _K_i = ints[:2]
-        keys_i = sorted(ints[2:])
+        keys_i = Counter(ints[2:])
         return T_i, keys_i
     chests = dict((i, read_chest()) for i in xrange(1, N + 1))
     return K, N, starting_keys, chests
