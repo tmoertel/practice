b03244cd56185d83ee4d460fcb3d0c0a1d7b6caf
Tom Moertel <tom@moertel.com>
2013-05-15 13:54:29 -0400

    Improve explanation of "Falling Diamonds" soln

--- google-code-jam/2013/round-1B-B-falling-diamonds/diamonds.py
+++ google-code-jam/2013/round-1B-B-falling-diamonds/diamonds.py
@@ -21,9 +21,9 @@ in the triangle's base.  These then form the related series,
                                                    ◆ ◆
                                        ◆          ◆ ◆ ◆
                                       ◆ ◆        ◆ ◆ ◆ ◆
-                             ◆       ◆ ◆ ◆      ◆ ◆ ◆ ◆ ◆
-                            ◆ ◆     ◆ ◆ ◆ ◆    ◆ ◆ ◆ ◆ ◆ ◆
-                     ◆     ◆ ◆ ◆   ◆ ◆ ◆ ◆ ◆  ◆ ◆ ◆ ◆ ◆ ◆ ◆
+                            ◆        ◆ ◆ ◆      ◆ ◆ ◆ ◆ ◆
+                           ◆ ◆      ◆ ◆ ◆ ◆    ◆ ◆ ◆ ◆ ◆ ◆
+                     ◆    ◆ ◆ ◆    ◆ ◆ ◆ ◆ ◆  ◆ ◆ ◆ ◆ ◆ ◆ ◆
 
 In closed form,
 
@@ -37,28 +37,32 @@ triangle, and the occupancy of (X, Y) is completely determined.  If
 (X, Y) is one of the triangle's diamonds, the probability we seek
 is 1; otherwise, it's 0.
 
-Looking at the problem more generally, when s[i] <= N < s[i+1], we
-have 3 mutually exclusive cases to consider:
+Looking at the problem more generally, when we find the largest i for
+which s[i] <= N, we know that s[i] <= N < s[i+1].  That is, any legal
+structure of N diamonds must at least contain the complete triangle i
+but cannot contain the complete triangle i+1.  Thus we have 3 mutually
+exclusive cases to consider:
 
-A.  (X, Y) is a diamond of triangle i.
-B.  (X, Y) is a not diamond of triangle i but is of triangle i+1.
-C.  (X, Y) is not even a diamond of triangle i+1.
+A.  (X, Y) is a diamond of triangle i, which the structure contains,
+    and therefore *must* be in the structure.
+
+B.  (X, Y) is a not diamond of triangle i but is of triangle i+1,
+    which contains the structure, and therefore *may* be in the
+    structure.
+
+C.  (X, Y) is not even a diamond of triangle i+1, and therefore
+    *cannot* be in the structure.
 
 Again, in cases A and C, there can be no uncertainty; they have
 respective probabilities of 1 and 0.
 
-But in case B, (X, Y) locates a diamond along the left or right edge
-of the *complete* triangle i+1, but since N < s[i+1], we know the
-structure in question is *not* a complete triangle.  Thus question
-then becomes, what is the probability that the diamond remains after
-s[i+1] - N diamonds have been removed at random from the complete
-triangle i+1 to make a structure of N diamonds?
+But in case B, (X, Y) locates a diamond within the complete triangle
+i+1 but not within the complete triangle i.  Thus (X, Y) must be
+within the difference between triangles i+1 and i, that is, along one
+of triangle i+1's sides.
 
-For example, for N = 8 we find the next-larger complete triangle of
-s[3] = 15 diamonds.  In case B, (X, Y) must be one of the white
-diamonds because it's within the complete triangle i = 3 but not
-within the next-smallest complete triangle i = 2 (shown as black
-diamonds).
+For example, when N = 8, (X, Y) must locate one of the white diamonds
+in the diagram below:
 
        ◇
       ◇ ◇         N      =  8
@@ -66,33 +70,50 @@ diamonds).
     ◇ ◆ ◆ ◇       s[i]   =  6
    ◇ ◆ ◆ ◆ ◇      s[i+1] = 15
 
-If X = 0, the probability must be 0 because the peak diamond must be
-missing from the structure since we're in case B for i = k (and not
-case A for i = k+1).
+But since N < s[i+i], we know that some of the white diamonds are
+missing from the structure.  At minimum the peak diamond is missing.
+Thus the question becomes, What is the probability that the white
+diamond at (X, Y) remains after s[i+1] - N white diamonds have been
+removed?
 
-For |X| > 0, the probability we seek is the probability that *fewer*
-than |X| diamonds have been removed from the side of the triangle that
-X is on.  (By symmetry we can ignore left-right distinctions and just
-refer to "X's side.")  The peak diamond is already gone, and now we
+If X = 0, the probability must be 0 because X = 0 can only locate a
+peak diamond, which we know our structure lacks.
+
+For |X| > 0, the probability we seek is the probability that, from X's
+side, the number of diamonds removed, let it be k, is less than |X|.
+(By symmetry we can ignore left-right distinctions.)  That is, k is a
+random variable, and we seek Pr(k < |X|).
+
+What is the distribution of k?  The peak is already gone, and now we
 must remove m = s[i+1] - N - 1 more diamonds, choosing at random
 between X's side and the other side.  When m < b[i+1], it's not
-possible to completely empty a side, and thus the underlying
-probabilities have a vanilla binomial distribution.  Thus our
-answer must be
+possible to completely empty a side, and thus k has a vanilla binomial
+distribution.  Therefore,
 
-    p = binom.cdf(abs(X) - 1, m, 0.5).
+    Pr(k < |X|)) = Pr(k <= |X| - 1)
+                 = binom.cdf(abs(X) - 1, m, 0.5).
 
-But when m >= b[i+1], it's easier to think about the dual problem of
-having to *add* N - s[i] < b[i+1] diamonds to the sides of the smaller
-triangle i.  The underlying distribution is again vanilla binomial,
-since it's not possible to over-fill a side.  In this formulation, we
-must add at least b[i+1] - |X| diamonds to X's side, which is the same
-as *not* adding fewer than b[i+1] - |X| diamonds.  Thus our
-answer must be
+But when m >= b[i+1], we can empty a side, and it's easier to think
+about the dual problem of having to *add* N - s[i] diamonds to the
+sides of the smaller triangle i.  Since N - s[i] < b[i+1] in this
+case, it's not possible to over-fill a side, and the distribution of
+the number of diamonds added to X's side, let it be j, is vanilla
+binomial.  In this formulation, we must add at least b[i+1] - |X|
+diamonds to X's side, which is the same as *not* adding fewer than
+b[i+1] - |X| diamonds.  Thus our answer must be
 
-    p = 1.0 - binom.cdf(b[i+1] - abs(X) - 1, N - s[i], 0.5).
+    Pr(k < |X|) = Pr(j >= b[i+1] - |X|)
+                = 1.0 - Pr(j < b[i+1] - |X|)
+                = 1.0 - Pr(j <= b[i+1] - |X| - 1])
+                = 1.0 - binom.cdf(b[i+1] - abs(X) - 1, N - s[i], 0.5).
 
-And that's it.
+And that's it.  For cases A and C, we know the answer immediately.
+For case B, we know the answer after spliting it into one of 3
+subcases:
+
+    (1) X = 0
+    (2) X != 0 and m < b[i+1]
+    (3) X != 0 and m >= b[i+1]
 
 """
 
@@ -116,11 +137,11 @@ def solve(problem):
         return 0.0  # case C
     # case B
     if X == 0:
-        return 0.0
+        return 0.0  # B(1)
     m = s(i + 1) - N - 1
     if m < b(i + 1):
-        return binom.cdf(abs(X) - 1, m, 0.5)
-    return 1.0 - binom.cdf(b(i + 1) - abs(X) - 1, N - s(i), 0.5)
+        return binom.cdf(abs(X) - 1, m, 0.5)  # B(2)
+    return 1.0 - binom.cdf(b(i + 1) - abs(X) - 1, N - s(i), 0.5)  # B(3)
 
 def is_triangle_diamond(x, y, i):
     """Test whether (x, y) locates a diamond in the complete triangle i."""
